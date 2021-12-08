117c8aeac33415686696d4116e94fd39bf9fd7e5
haka08 <91915361+haka08@users.noreply.github.com>
2021-12-09 00:12:39 +0800

    Listing recipes names from cookbook

--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -14,6 +14,14 @@ describe('Cookbook', () => {
   describe('Listing recipes', () => {
     test('should allow the recipes to be listed', () => {
 
+     const myCookbook = new Cookbook();
+      myCookbook.addRecipe('salad', ['tomato', 'lettuce', 'eggs', 'cucumber']);
+      myCookbook.addRecipe('cookie',['flour','milk','eggs']);
+
+      const myReceipes = myCookbook.listRecipes();
+
+      expect(myReceipes).toEqual(['salad','cookie']);
+
     });
   });
 
